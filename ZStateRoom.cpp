b88676d3844fd3763b8c320b5a85c2f6c8b9b8ae
JoeThomas490 <b4002810@my.shu.ac.uk>
2018-01-07 16:38:35 +0000

    Added start/stop control in main loop when '.' is pressed.  Created ZStateRoom class for room behaviour (not implemented just yet).  Added ability to stop motors by pressing spacebar when in USER state.

---
+++ ZStateRoom.cpp
@@ -0,0 +1,21 @@
+// 
+// 
+// 
+
+#include "ZStateRoom.h"
+
+ZStateRoom::~ZStateRoom()
+{
+}
+
+void ZStateRoom::InitState()
+{
+}
+
+void ZStateRoom::UpdateState()
+{
+}
+
+void ZStateRoom::StopState()
+{
+}
